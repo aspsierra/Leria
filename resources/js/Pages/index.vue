be3f3a335bde8ae5b5fe29a54dcfa09dc507d7b4
Álvaro Sierra <asp.sierra@gmail.com>
2023-10-06 18:38:18 +0200

    created publish post form

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,31 +1,41 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 import GuestLayoutVue from '@/Layouts/GuestLayout.vue';
 import UserLayout from '@/Layouts/UserLayout.vue';
+import PublishPostLarge from '../components/Forms/PublishPostLarge.vue'
 import { ref } from 'vue';
 
 import axios from 'axios';
+import TextInput from '@/Components/TextInput.vue';
 
 const props = defineProps({
     user: Object
 })
 
-const logout = () =>{
+const logout = () => {
     axios.post('/logout')
 }
 
+const form = useForm({
+    post: ''
+})
+
 </script>
 <template>
-    <head title='Inicio' />
-    <component :is="user === null ? GuestLayoutVue : UserLayout" :avatar = "user.profile_pic"
-    class="container mx-auto"/>
-    <div class="container mx-auto">
-        <p>Benvido </p><p v-if=" user !== null">{{ user.name }}</p>
-        <div class="avatar">
-            <div class="w-24 rounded">
-                <img :src="'/storage/' + user.profile_pic "/>
+    <Head title='Inicio' />
+    <component :is="user === null ? GuestLayoutVue : UserLayout" :avatar="user.profile_pic" class="container mx-auto" />
+    <div class="container mx-auto flex flex-row gap-1 mt-1">
+        <section class="bg-red-400 w-1/4">
+            ssss
+        </section>
+        <section class="bg-green-700 w-2/4">
+
+            <div class="hidden lg:block bg-purple-300">
+                <PublishPostLarge :pic="user.profile_pic"/>
             </div>
-        </div>
-        <button class="btn btn-accent" @click="logout">LOgout</button>
+        </section>
+        <section class="bg-blue-700 w-1/4">
+            ssss
+        </section>
     </div>
 </template>
---
+++ resources/js/components/Forms/PublishPostLarge.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Head, Link, useForm } from '@inertiajs/vue3';
+const props = defineProps({
+    pic:String,
+})
+const form = useForm({
+    post: ''
+})
+</script>
+<template>
+    <form class="p-4" @submit.prevent="">
+        <div class="flex flex-row gap-2 w-full">
+            <div class="avatar">
+                <div class="w-20 rounded-full">
+                    <img :src="'/storage/' + pic" />
+                </div>
+            </div>
+
+            <div class="w-full">
+                <InputLabel for="post" value="Nombre de usuario" />
+                <textarea class="textarea textarea-bordered w-full max-h-xs resize-none" placeholder="Que estás argallando?"
+                    maxlength="140" rows="2" />
+            </div>
+        </div>
+        <div class="w-full flex justify-end">
+            <button class="btn btn-primary">Publicar</button>
+        </div>
+    </form>
+</template>
